b6e9f4a17922c2f1889a8e0d071707b4ef538a61
Vladimir <w112vovan@mail.ru>
2023-11-27 15:26:15 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L18_bracketExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentDepth = 0;
+            int maxDepth = 0;
+            char openBracket = '(';
+            char closedBracket = ')';
+            bool correctAction = true;
+            string userInput;
+
+            Console.Write("Введите скобочное выражение: ");
+            userInput = Console.ReadLine();
+
+            foreach (char symbol in userInput)
+            {
+                if (symbol == openBracket)
+                {
+                    currentDepth++;
+
+                    if (currentDepth > maxDepth)
+                        maxDepth = currentDepth;
+                }
+                else if (symbol == closedBracket)
+                {
+                    if (currentDepth > 0)
+                        currentDepth--;
+                    else
+                    {
+                        correctAction = false;
+                        break;
+                    }
+                }
+            }
+            if (correctAction && currentDepth == 0)
+                Console.WriteLine($"\nДанное скобочное выражение корректно.\nМаксимальная глубина вложенности составляет {maxDepth}\n");
+            else
+                Console.WriteLine($"\nДанное скобочное выражение некорректно.");
+        }
+    }
+}
