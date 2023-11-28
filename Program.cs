38e7244a57491fcb154864d4e253827b4e459b26
Vladimir <w112vovan@mail.ru>
2023-11-29 00:46:42 +0500

    Правка 4

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace L18_bracketExpression
             int maxDepth = 0;
             char openBracket = '(';
             char closedBracket = ')';
-            bool correctAction = true;
             string userInput;
 
             Console.Write("Введите скобочное выражение: ");
@@ -27,25 +26,18 @@ namespace L18_bracketExpression
                     currentDepth++;
 
                     if (currentDepth > maxDepth)
-                    {
                         maxDepth = currentDepth;
-                    }
                 }
                 else if (symbol == closedBracket)
                 {
-                    if (currentDepth > 0)
-                    {
-                        currentDepth--;
-                    }
-                    else
-                    {
-                        correctAction = false;
+                    currentDepth--;
+
+                    if (currentDepth < 0)
                         break;
-                    }
                 }
             }
 
-            if (correctAction && currentDepth == 0)
+            if (currentDepth == 0)
                 Console.WriteLine($"\nДанное скобочное выражение корректно.\nМаксимальная глубина вложенности составляет {maxDepth}\n");
             else
                 Console.WriteLine($"\nДанное скобочное выражение некорректно.");
